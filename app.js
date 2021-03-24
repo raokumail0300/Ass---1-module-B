f8fac6ec38cb4fa507daf2cad81c706e414f6892
raokumail0300 <75995834+raokumail0300@users.noreply.github.com>
2021-03-24 17:10:07 +0500

    complete

---
+++ app.js
@@ -0,0 +1,24 @@
+
+                        //    Q1
+
+
+let arr1 = ["kumail","ali","hassan",["v1","v2","v3"]];
+let [n1,n2,n3,[m1,,m2]] = arr1;
+console.log(n1,n2,n3,m1);
+
+
+
+                      //    Q2
+
+
+let student = {
+    name: "Rao kumail",
+    rollNo: 10,
+    phoneNo: 111244622
+};
+
+
+
+console.log(Object.keys(student));
+console.log(Object.values(student));
+
